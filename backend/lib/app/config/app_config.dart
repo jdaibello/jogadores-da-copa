1d13cd861ee322cacacdc3eaf86c6ef6508d1401
João Pedro <jpedrodaibello1999@outlook.com>
2022-12-04 15:53:37 -0300

    [BACKEND] Setting log structure

--- backend/lib/app/config/app_config.dart
+++ backend/lib/app/config/app_config.dart
@@ -1,9 +1,16 @@
 import 'package:dotenv/dotenv.dart';
+import 'package:get_it/get_it.dart';
+import 'package:jogadores_da_copa_api/app/logger/logger.dart';
+import 'package:jogadores_da_copa_api/app/logger/logger_impl.dart';
 
 class AppConfig {
   Future<void> loadAppConfig() async {
     await _loadEnv();
+    _configureLogger();
   }
 
   Future<void> _loadEnv() async => DotEnv()..load();
+
+  void _configureLogger() =>
+      GetIt.I.registerLazySingleton<Logger>(() => LoggerImpl());
 }
---
+++ backend/lib/app/logger/logger_impl.dart
@@ -0,0 +1,22 @@
+import 'package:jogadores_da_copa_api/app/logger/logger.dart';
+import 'package:logger/logger.dart' as log;
+
+class LoggerImpl implements Logger {
+  final _logger = log.Logger();
+
+  @override
+  void debug(message, [error, StackTrace? stackTrace]) =>
+      _logger.d(message, error, stackTrace);
+
+  @override
+  void error(message, [error, StackTrace? stackTrace]) =>
+      _logger.e(message, error, stackTrace);
+
+  @override
+  void info(message, [error, StackTrace? stackTrace]) =>
+      _logger.i(message, error, stackTrace);
+
+  @override
+  void warning(message, [error, StackTrace? stackTrace]) =>
+      _logger.w(message, error, stackTrace);
+}
